ee438be48641af3c6a9f565be0cf3e786d1a5388
Aravind Krishna <akaravindk59@gmail.com>
2022-06-02 03:00:10 +0530

    make it better

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,7 +14,7 @@ const auth = async (req, res, next) => {
 
       req.userId = decodedData?.id
     } else {
-      decodedData = jwt.decode(token)
+      decodedData = decode(token)
 
       req.userId = decodedData?.sub
     }
